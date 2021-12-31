baaa80b6b2c6f47b473185e2bb3b2b4011d86a5a
fredesmau <94102432+fredesmau@users.noreply.github.com>
2021-12-30 22:14:25 -0300

    Add files via upload

---
+++ portfolio-main/front-end/script.js
@@ -0,0 +1,48 @@
+// Mostrar y ocultar el menú
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close');
+
+// Mostrar
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show');
+});
+
+// Ocultar
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show');
+});
+
+// Quitar el menú
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction()
+{
+    navMenu.classList.remove('show');
+};
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Scroll actions active link
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', scrollActive);
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight)
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active')
+        }
+        else
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active')
+        }
+    })
+}
